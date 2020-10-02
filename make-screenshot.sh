b51be914851eefcead1e244411c03613b2990c49
Rishi Vishwakarma <68197457+VishwakarmaRishi@users.noreply.github.com>
2020-10-02 13:38:02 +0530

    Update make-screenshot.sh

--- make-screenshot.sh
+++ make-screenshot.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 # Copyright 2017 The Tectonic Project
 # Licensed under the MIT License
-#
+
 # This script assumes that the main `tectonic` repository is in a directory
 # next to this one. It requires the "aha" program to be available,
 # https://github.com/theZiz/aha .
 
-ttdir=../tectonic
-aha=$HOME/sw/aha/aha
+ttdir = ../tectonic
+aha = $HOME/sw/aha/aha
 
 set -e
 
